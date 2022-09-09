10c3cab2372800ce2286da8286e6ce9dafd822bf
Alina Vorontsova <vorontsova-alina@mail.ru>
2022-09-09 13:31:39 +0300

    additional_hw_stackoverflow update

--- additional_hw_stackoverflow.py
+++ additional_hw_stackoverflow.py
@@ -1,5 +1,6 @@
 import requests 
-from pprint import pprint
+
+
 def get_news():
     url = 'https://api.stackexchange.com/2.3/questions?fromdate=1662508800&todate=1662681600&order=desc&sort=activity&tagged=python&site=stackoverflow'
     response = requests.get(url).json()
